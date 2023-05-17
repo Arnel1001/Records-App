687874ceaa0ed5dd76d9ae5ee2c514c3e505e599
Arnel1001 <202080475@psu.palawan.edu.ph>
2023-05-17 23:58:10 +0800

    until emplooyee-add yet

---
+++ recordsapp/config/config.php
@@ -0,0 +1,6 @@
+<?php
+    define('ROOT_URL', 'http://localhost/phpmyadmin/index.php?route=/database/sql&db=recordapp_db');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'recordapp_db');
